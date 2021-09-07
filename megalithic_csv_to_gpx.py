2222c36a97a4a57d268dec6e2b9e731527c0e5c3
howff <howff>
2021-09-07 16:03:04 +0100

    Use pygeodesy because OSGridConverter gives wrong answers

--- megalithic_csv_to_gpx.py
+++ megalithic_csv_to_gpx.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from OSGridConverter import grid2latlong
+#from OSGridConverter import grid2latlong # Doesn't return correct lat,lon
+from pygeodesy import osgr
 import csv
 import sys
+
+region_name = 'Harris'
 
 def hdr(fd):
     print("""<?xml version="1.0" encoding="UTF-8" ?>
@@ -14,18 +17,22 @@ creator="Memory-Map 6.3.3.1261 https://memory-map.com"
  xmlns:xgarmin="http://www.garmin.com/xmlschemas/GpxExtensions/v3"
  xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.topografix.com/GPX/gpx_style/0/2 http://www.topografix.com/GPX/gpx_style/0/2/gpx_style.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 https://www8.garmin.com/xmlschemas/GpxExtensionsv3.xsd">""", file=fd)
 
+ofd = sys.stdout
+
 with open('megalithic.csv') as fd:
     rdr = csv.reader(fd, delimiter='|')
     rownum = 0
     for row in rdr:
         rownum += 1
         if rownum == 1:
-            hdr(sys.stdout)
+            hdr(ofd)
             continue
-        ll = grid2latlong(row[1])
-        print('<wpt lat="%s" lon="%s">' % (ll.latitude, ll.longitude))
-        print(' <name><![CDATA[%s]]></name>' % row[0])
-        print(' <sym>Flag</sym>')
-        print(' <type>Marks:Megalithic:Harris</type>')
-        print('</wpt>')
-print('</gpx>')
+        #ll = grid2latlong(row[1])
+        #print('%s -> %s %s', (row[1], ll.latitude, ll.longitude))
+        ll = osgr.parseOSGR(row[1]).toLatLon()
+        print('<wpt lat="%s" lon="%s">' % (ll[0], ll[1]), file=ofd)
+        print(' <name><![CDATA[%s]]></name>' % row[0], file=ofd)
+        print(' <sym>Flag</sym>', file=ofd)
+        print(' <type>Marks:Megalithic:%s</type>' % region_name, file=ofd)
+        print('</wpt>', file=ofd)
+print('</gpx>', file=ofd)
